cdc0803ec60e05b2cceb1561ee9d43ed031e2a3c
Peter Anema <7320173+PeterAnema@users.noreply.github.com>
2021-04-11 12:37:11 +0200

    Added base controller * @RestController * @GetMapping

--- src/main/java/nl/gettoworktogether/springboot_step_by_step_application/SpringbootStepByStepApplication.java
+++ src/main/java/nl/gettoworktogether/springboot_step_by_step_application/SpringbootStepByStepApplication.java
@@ -2,12 +2,20 @@ package nl.gettoworktogether.springboot_step_by_step_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootStepByStepApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootStepByStepApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello world!";
     }
 
 }
